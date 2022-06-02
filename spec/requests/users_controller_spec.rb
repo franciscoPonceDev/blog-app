7691785ea82167ca0c2f495ddc23aef4d6cf464e
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-06-02 16:49:04 -0300

    add controller tests

---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Users Controller', type: :request do
+  describe 'Index Action' do
+    before(:each) do
+      get '/users/'
+    end
+
+    it 'Response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'Show Action' do
+    before(:each) do
+      get '/users/:id'
+    end
+
+    it 'Response status is correct' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
