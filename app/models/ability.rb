26418d1b03481309a7ac8b7ea40b5b05a22698a5
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-14 22:12:17 +0100

    Manage authorization.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,10 +6,11 @@ class Ability
   def initialize(user)
     if user.admin?
       can :manage, :all
-    elseif user.is? :default
-      can :manage, Post, user: user.id
-      can :manage, Comment, user: user.id
-      can :manage, Like, user: user.id
+    elsif user.is? :default
+      can :read, :all
+      can :manage, Post, user_id: user.id
+      can :manage, Comment, user_id: user.id
+      can :manage, Like, user_id: user.id
     else
       can :read, :all
     end
