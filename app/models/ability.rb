e346e065bb9a0d3b7bc161312fe02bd3e4a1b7c7
yersel500 <yersel500@gmail.com>
2022-06-14 17:16:24 -0500

    add ability to destroy comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,8 +8,9 @@ class Ability
       can :manage, :all
     elsif user.is? :default
       can :read, :all
+      can :create, Comment
+      can :destroy, Comment, user_id: user.id
       can :manage, Post, user_id: user.id
-      can :manage, Comment, user_id: user.id
       can :manage, Like, user_id: user.id
     else
       can :read, :all
