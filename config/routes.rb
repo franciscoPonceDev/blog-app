564c9027323672f9ec61a635aa3e2f1bcc912983
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-14 22:12:46 +0100

    Add destroy route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
     end
