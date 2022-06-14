3bfc235e378dc282f2ac17bc94a35b86f86a1f0c
yersel500 <yersel500@gmail.com>
2022-06-14 17:17:08 -0500

    add button to logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
   devise_for :users
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:create]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[create destroy]
       resources :likes, only: [:create]
     end
   end
@@ -10,5 +14,5 @@ Rails.application.routes.draw do
   post 'users/:user_id/posts', to: 'posts#create', as: 'posts'
   post 'users/:user_id/posts/:post_id/likes', to: 'likes#create', as: 'likes'
 
-   root "users#index"
+  root 'users#index'
 end
